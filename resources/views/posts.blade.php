5cce27d81038f95803215ef5bf509795f69ff001
billywithflutter <billy89square@gmail.com>
2023-01-20 21:06:02 +0700

    making the navigation work and going to try to made the Navigation Bar

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Billy | Posts</title>
+    <title>Billy | BLog</title>
 </head>
 <body>
-    <h1>Halaman Post</h1>
+    <h1>Halaman Blog</h1>
 </body>
 </html>
